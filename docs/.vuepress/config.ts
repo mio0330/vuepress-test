8f6b95b45d3afcb7885df5072d3e9d7ecabd9ad7
turnback <274114318@qq.com>
2023-02-12 15:14:34 +0800

    first commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,13 @@
+import { defaultTheme } from 'vuepress'
+
+export default {
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+    ],
+  }),
+}
